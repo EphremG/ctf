9fff72c64f1a7cd088ab007f9f7f9fd73277ca19
EphremG <fstudioceh@gmail.com>
2018-11-01 10:06:22 +0200

    Update comment.php

--- comment.php
+++ comment.php
@@ -3,28 +3,32 @@ session_start();
 error_reporting(0);
 include('includes/config.php');
 
-$username =$_SESSION['sess_username'];
+$username = $_SESSION['sess_username'];
+//echo $_SESSION['sess_studentId'];
+$StudentId = $_SESSION['sess_studentId'];
+//$StudentId = 1;
+//echo $StudentId;
 if(strlen($_SESSION['alogin'])=="")
     {   
     header("Location: index.php"); 
     } 
-elseif($_SESSION['sess_userrole']!="student"){
+elseif($_SESSION['sess_userrole']!="student"){         
     header("Location: index.php"); 
 }
 else{
     if(isset($_POST['submit'])){
         $name = $_POST['name'];
         $comment = $_POST['comment'];
-
-        $sql="insert into comment(name,comment) values (:name,:comment)";
+        $sql="insert into comment(studentId,comment,name) values (:StudentId, :comment, :name)";
         $addcomment = $dbh->prepare($sql);
-        $addcomment-> bindParam(':name', $name, PDO::PARAM_STR);
+        $addcomment-> bindParam(':StudentId', $StudentId, PDO::PARAM_INT);
         $addcomment-> bindParam(':comment', $comment, PDO::PARAM_STR);
+        $addcomment-> bindParam(':name', $name, PDO::PARAM_STR);
         $addcomment->execute();
+        //print_r($this->pdo->errorInfo());
         $msg="Your Comment succesfully Posted";
     }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -56,39 +60,61 @@ else{
                     <div class="main-page">
                         <div class="container-fluid">
                             <div class="row page-title-div">
+                            <h2 class="title">WELCOME TO STUDENTS BLOG PAGE</h2>
                                 <div class="col-sm-6">
-                                    <h2 class="title">Dashboard</h2>
+                                   
                                     <center>
                                     <?php if($msg){?>
                                         <div class="alert alert-success left-icon-alert" role="alert">
-                                        <strong>Well done!</strong><?php echo htmlentities($msg); ?>
+                                        <a href="#" class="close" data-dismiss="alert">&times;</a>
+                                        <strong></strong><?php echo htmlentities($msg); ?>
                                         </div><?php } ?>
-                                        <form action="comment.php" method="POST">
-                                        <table>
-                                        <br><input type="hidden" value="<?php echo $username; ?>" name="name"/></td></tr>
-                                        <tr><td colspan="2">Comment: </td></tr>
-                                        <tr><td colspan="5"><textarea name="comment"  rows="5" cols="30"></textarea></td></tr>
-                                        <tr><td colspan="2"><input type="submit" name="submit" value="Comment"></td></tr>
-                                        </table>
-                                        </form>
-                                        <?php 
-                                        $query = "SELECT * from comment ";
-                                        $stmt = $dbh->prepare($query);
-                                        $stmt->execute();
-                                        $resultss=$stmt->fetchAll(PDO::FETCH_OBJ);
-                                        $cnt=1;
-                                        if($stmt->rowCount() > 0)
-                                        {
-                                        foreach($resultss as $row)
-                                        {  
-                                        $id = $row->Id;
-                                        ?>
-                                        <p><b> <?php echo htmlentities($row->Name);?> :</b> <?php echo htmlentities($row->Comment);
-                                        if($_SESSION['sess_username'] == $row->Name){?>
-                                        <a href='deletecomment.php?id=<?php echo $id;?>'>Delete</a>
-                                       <?php }                
-                                         }} ?>
-                                                      
+                                        <div class="form-group">
+                                            <form action="comment.php" method="POST">
+                                            <table>
+                                            <br><input type="hidden" value="<?php echo $username; ?>" name="name"/></td></tr>
+                                            <label for="comment">Comment:</label>
+                                            <textarea class="form-control" name="comment" rows="5" id="comment"></textarea>
+                                            <tr><td colspan="2"><input type="submit" name="submit" value="Post"></td></tr>
+                                            </table>
+                                            </form> 
+                                        </div>
+                                                                                                                 
+                                    
+                                        </div>
+                                        <table class="table table-hover">     
+                                         <tbody>
+                                                    <?php 
+                                                        $query = "SELECT * from comment ";
+                                                        $stmt = $dbh->prepare($query);
+                                                        $stmt->execute();
+                                                        $resultss=$stmt->fetchAll(PDO::FETCH_OBJ);
+                                                        $cnt=1;
+                                                        if($stmt->rowCount() > 0)
+                                                        {
+                                                        foreach($resultss as $row)
+                                                        {  
+                                                        $commentid = $row->id;
+                                                    ?>
+                                            <tr>
+                                                <td><b><?php echo strtoupper($row->Name);?>:</b> </td>
+                                                <td><?php echo $row->comment;?> </td>  
+                                                <td> <?php  if($StudentId == $row->studentId){?>
+                                                            <a href='deletecomment.php?commentid=<?php echo $commentid;?>'><button type="button" class="btn btn-danger">Delete</button></a></td>
+                                                
+                                                    <?php } ?>     
+                                               
+                                       <?php  }  ?>
+                                       </tr>   
+                                        </tbody>
+                                        </table>  
+                                    <div>   <?php } else {
+                                         echo '<div class="alert alert-success left-icon-alert" role="alert"> ';
+                                          echo ' <a href="#" class="close" data-dismiss="alert">&times;</a> ';
+                                           echo ' <strong></strong>Well done! The flag is Y0U_AR3_7H3_B357 ';
+                                         }                                        
+                                         ?>
+                                       </div>               
                                 </div>
                                 <!-- /.col-sm-6 -->
                             </div>
