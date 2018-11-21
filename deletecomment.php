2bebccd681bdf195f7d21e694adce6b87aa442ff
EphremG <fstudioceh@gmail.com>
2018-11-21 14:27:07 +0200

    Update deletecomment.php

--- deletecomment.php
+++ deletecomment.php
@@ -2,21 +2,35 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
+
 if(strlen($_SESSION['alogin'])=="")
     {   
     header("Location: index.php"); 
     }
 else{
-    $id = $_GET['commentid'];
-    if($id != ""){
-        $sql = "delete from comment where id=:id";
+    $studentId = $_SESSION['sess_studentId']; //This is the loggedin student ID value
+    $id = $_GET['commentid']; // Comment ID from URL in GET Method
+   // echo $id;
+     if($id != ""){
+        $sql = "delete from comment where id=:id ";
+        //solutions
+        //$sql = "delete from comment where id=:id and studentId=:studentId";
         $deletecomment = $dbh->prepare($sql);
-        $deletecomment-> bindParam(':id', $id, PDO::PARAM_INT);
+        $deletecomment-> bindParam(':id', $id);
+        //$deletecomment-> bindParam(':studentId', $studentId);
         $deletecomment->execute();
+        if($deletecomment != 0){
         $msg="Comment removed Succesfully! ";
         header("Location: comment.php");
+        }
+        else {
+            $msg="You are not allowed to delete! ";
+            header("Location: comment.php");
+        }
+
     }
     else {
+        $msg="You are not allowed to delete! ";
         header("Location: comment.php");
     }
 }
