8253e325432a69952c6eee4c06eed3c578c8f57a
EphremG <fstudioceh@gmail.com>
2018-11-22 16:26:24 +0200

    Update find-result.php

--- find-result.php
+++ find-result.php
@@ -16,11 +16,8 @@ if(strlen($_SESSION['alogin'])=="")
     	<meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Grade Management System</title>
         <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" >
-        <link rel="stylesheet" href="css/font-awesome.min.css" media="screen" >
         <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen" >
-        <link rel="stylesheet" href="css/icheck/skins/flat/blue.css" >
         <link rel="stylesheet" href="css/main.css" media="screen" >
-        <script src="js/modernizr/modernizr.min.js"></script>
     </head>
     <body class="">
             <div class="main-wrapper">
@@ -95,28 +92,6 @@ foreach($results as $result)
         </div>
         <!-- /.main-wrapper -->
 
-        <!-- ========== COMMON JS FILES ========== -->
-        <script src="js/jquery/jquery-2.2.4.min.js"></script>
-        <script src="js/jquery-ui/jquery-ui.min.js"></script>
-        <script src="js/bootstrap/bootstrap.min.js"></script>
-        <script src="js/pace/pace.min.js"></script>
-        <script src="js/lobipanel/lobipanel.min.js"></script>
-        <script src="js/iscroll/iscroll.js"></script>
-
-        <!-- ========== PAGE JS FILES ========== -->
-        <script src="js/icheck/icheck.min.js"></script>
-
-        <!-- ========== THEME JS ========== -->
-        <script src="js/main.js"></script>
-        <script>
-            $(function(){
-                $('input.flat-blue-style').iCheck({
-                    checkboxClass: 'icheckbox_flat-blue'
-                });
-            });
-        </script>
-
-        <!-- ========== ADD custom.js FILE BELOW WITH YOUR CHANGES ========== -->
     </body>
 </html>
         <?php }?>
