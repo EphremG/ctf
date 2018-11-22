0ee83c008d70189ad5e76f935f929f9a4c997059
EphremG <fstudioceh@gmail.com>
2018-11-22 16:28:15 +0200

    Update result.php

--- result.php
+++ result.php
@@ -21,7 +21,6 @@ if(strlen($_SESSION['alogin'])=="")
         <link rel="stylesheet" href="css/lobipanel/lobipanel.min.css" media="screen">
         <link rel="stylesheet" href="css/prism/prism.css" media="screen" >
         <link rel="stylesheet" href="css/main.css" media="screen" >
-        <script src="js/modernizr/modernizr.min.js"></script>
     </head>
     <body>
        <div class="main-wrapper">
@@ -184,15 +183,5 @@ echo htmlentities("Invalid Roll Id");
 
         </div>
         <!-- /.main-wrapper -->
-
-        <!-- ========== COMMON JS FILES ========== -->
-        <script src="js/jquery/jquery-2.2.4.min.js"></script>
-        <script src="js/bootstrap/bootstrap.min.js"></script>
-        <script src="js/pace/pace.min.js"></script>
-        <script src="js/lobipanel/lobipanel.min.js"></script>
-        <script src="js/iscroll/iscroll.js"></script>
-
-        <!-- ========== PAGE JS FILES ========== -->
-        <script src="js/prism/prism.js"></script>
     </body>
         </html> <?php } ?>
