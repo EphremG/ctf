6e68c02ef6c114a006ea229a1e1d90561880f4b3
EphremG <fstudioceh@gmail.com>
2018-11-22 16:49:05 +0200

    Update comment.php

--- comment.php
+++ comment.php
@@ -29,27 +29,16 @@ else{
         $msg="Your Comment succesfully Posted";
     }
 ?>
-<!DOCTYPE html> 
-<html lang="en"> 
-    <head> 
-        <meta charset="utf-8"> 
-        <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
-    	<meta name="viewport" content="width=device-width, initial-scale=1"> 
-        <title>Grade Management System | Dashboard</title> 
-        <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" > 
-        <link rel="stylesheet" href="css/font-awesome.min.css" media="screen" > 
-        <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen" > 
-        <link rel="stylesheet" href="css/lobipanel/lobipanel.min.css" media="screen" > 
-        <link rel="stylesheet" href="css/toastr/toastr.min.css" media="screen" > 
-        <link rel="stylesheet" href="css/icheck/skins/line/blue.css" >  
-        <link rel="stylesheet" href="css/icheck/skins/line/red.css" > 
-        <link rel="stylesheet" href="css/icheck/skins/line/green.css" > 
-        <link rel="stylesheet" href="css/main.css" media="screen" > 
-        <script src="js/modernizr/modernizr.min.js"></script> 
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Grade Management System | Dashboard</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" >
+         <link rel="stylesheet" href="css/main.css" media="screen" >
 
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
     <body class="top-navbar-fixed">
         <div class="main-wrapper">
@@ -79,8 +68,9 @@ else{
                                             </table>
                                             </form> 
                                         </div>
-                                                                                                                 
-                                    
+                                        <form method="POST" >
+                                        <input type="hidden" value="<?php echo $_SESSION['sess_studentId'];?>" name="LoggedinStudentId">                                                                     
+                                        </form>
                                         </div>
                                         <table class="table table-hover">     
                                          <tbody>
