854d62c78600c769a4547b9abb2ef37048dff78e
EphremG <fstudioceh@gmail.com>
2018-11-24 13:17:58 +0200

    Update studentdashboard.php

--- studentdashboard.php
+++ studentdashboard.php
@@ -22,18 +22,10 @@ else{
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    	<meta name="viewport" content="width=device-width, initial-scale=1">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Grade Management System | Dashboard</title>
         <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" >
-        <link rel="stylesheet" href="css/font-awesome.min.css" media="screen" >
-        <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen" >
-        <link rel="stylesheet" href="css/lobipanel/lobipanel.min.css" media="screen" >
-        <link rel="stylesheet" href="css/toastr/toastr.min.css" media="screen" >
-        <link rel="stylesheet" href="css/icheck/skins/line/blue.css" >
-        <link rel="stylesheet" href="css/icheck/skins/line/red.css" >
-        <link rel="stylesheet" href="css/icheck/skins/line/green.css" >
         <link rel="stylesheet" href="css/main.css" media="screen" >
-        <script src="js/modernizr/modernizr.min.js"></script>
     </head>
     <body class="top-navbar-fixed">
         <div class="main-wrapper">
@@ -149,62 +141,7 @@ $totalresults=$query3->rowCount();
         </div>
         <!-- /.main-wrapper -->
 
-        <!-- ========== COMMON JS FILES ========== -->
-        <script src="js/jquery/jquery-2.2.4.min.js"></script>
-        <script src="js/jquery-ui/jquery-ui.min.js"></script>
-        <script src="js/bootstrap/bootstrap.min.js"></script>
-        <script src="js/pace/pace.min.js"></script>
-        <script src="js/lobipanel/lobipanel.min.js"></script>
-        <script src="js/iscroll/iscroll.js"></script>
-
-        <!-- ========== PAGE JS FILES ========== -->
-        <script src="js/prism/prism.js"></script>
-        <script src="js/waypoint/waypoints.min.js"></script>
-        <script src="js/counterUp/jquery.counterup.min.js"></script>
-        <script src="js/amcharts/amcharts.js"></script>
-        <script src="js/amcharts/serial.js"></script>
-        <script src="js/amcharts/plugins/export/export.min.js"></script>
-        <link rel="stylesheet" href="js/amcharts/plugins/export/export.css" type="text/css" media="all" />
-        <script src="js/amcharts/themes/light.js"></script>
-        <script src="js/toastr/toastr.min.js"></script>
-        <script src="js/icheck/icheck.min.js"></script>
-
-        <!-- ========== THEME JS ========== -->
-        <script src="js/main.js"></script>
-        <script src="js/production-chart.js"></script>
-        <script src="js/traffic-chart.js"></script>
-        <script src="js/task-list.js"></script>
-        <script>
-            $(function(){
-
-                // Counter for dashboard stats
-                $('.counter').counterUp({
-                    delay: 10,
-                    time: 1000
-                });
-
-                // Welcome notification
-                toastr.options = {
-                  "closeButton": true,
-                  "debug": false,
-                  "newestOnTop": false,
-                  "progressBar": false,
-                  "positionClass": "toast-top-right",
-                  "preventDuplicates": false,
-                  "onclick": null,
-                  "showDuration": "300",
-                  "hideDuration": "1000",
-                  "timeOut": "5000",
-                  "extendedTimeOut": "1000",
-                  "showEasing": "swing",
-                  "hideEasing": "linear",
-                  "showMethod": "fadeIn",
-                  "hideMethod": "fadeOut"
-                }
-                toastr["success"]( "Welcome to Grade Management System!");
-
-            });
-        </script>
+  
     </body>
 </html>
 <?php } ?>
